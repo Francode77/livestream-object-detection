e48377fe901d7334666de0cab55fdd891ccabb06
Francode77 <francodefall@gmail.com>
2023-03-22 12:31:53 +0100

    app update

--- youtube_od.py
+++ youtube_od.py
@@ -6,106 +6,111 @@ import torchvision
 import torch 
 import time 
 import wget
-     
-@st.cache
-def loadModel():
-    start_dl = time.time()
-    model_file = wget.download('https://github.com/Francode77/livestream-object-detection/blob/main/model/yolov5s.pt', out="model/")
-    finished_dl = time.time()
-    print(f"Model Downloaded, ETA:{finished_dl-start_dl}")
-loadModel()
+import socket   
+
+# Determine on which platform the app runs, for model loading
+def check_host():
+    hostname=socket.gethostname()   
+    IPAddr=socket.gethostbyname(hostname)     
+    if '192.168.' in IPAddr:
+        host='local'
+    else:
+        host='streamlit_cloud'
+    return host
+
+def wait_area():
+    with st.empty(): 
+        while True:
+            st.write(f"⏳ Please choose a stream")
+            time.sleep(0.31)
+            st.write(f"⏳ Please choose a stream .")
+            time.sleep(0.31)
+            st.write(f"⏳ Please choose a stream ..")
+            time.sleep(0.31)
+            st.write(f"⏳ Please choose a stream ...")
+            time.sleep(0.31)
+            st.write(f"⏳ Please choose a stream ....")
+
+# Function to display video from button_click
+def open_stream(x,stream_link,title):
+    st.session_state.stream_link=stream_link
+    st.subheader(f"Stream {x+1}")
+    st.text(title)
+    display_video(model,stream_link)
+
+# Function to draw the sidebar
+def draw_sidebar():
+    thumbs=[]
+    videos=[]
+    titles=[] 
+    urls=[
+    'https://www.youtube.com/watch?v=hIJs5p7ND0g',
+    'https://www.youtube.com/watch?v=1-iS7LArMPA',
+    'https://www.youtube.com/watch?v=CvOB-Is_yYU'
+    ]
+
+    for x,url in enumerate(urls):
+        # Use pafy to get videostream from youtube
+        videos.append(pafy.new(url))
+
+        # Getting thumbnail of the video
+        thumbs.append(videos[x].thumb)
+        titles.append(videos[x].title) 
+
+    # Create a sidebar for overview    
+    st.sidebar.title("Youtube streams") 
+    for x, (thumb, title) in enumerate(zip(thumbs,titles)):
+        st.sidebar.image(thumb) 
+        st.sidebar.button(f"Stream {x+1}",on_click=open_stream,kwargs=dict(x=x,stream_link=urls[x],title=title))
 
 
-variable = None
+# Initial run will not have model, so declare it here
+if 'model' not in st.session_state:
+    
+    # Use GPU if available, else use CPU
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    boxes = torch.tensor([[0., 1., 2., 3.]]).to(device)
+    scores = torch.randn(1).to(device)
+    iou_thresholds = 0.5
+
+    # YoloV5
+    # Check whether the app is run locally or on streamlit cloud,
+    # On streamlit cloud we have to download the model because of permission errors
+    if check_host()=='local':
+        # load pretrained model locally
+        model = yolov5.load('/model/yolov5s.pt')
+    else:
+        # load pretrained model on streamlit
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/yolov5s.pt', force_reload=True) 
+    #model.to(device)   
+    st.session_state['model'] = model
+    draw_sidebar()
+
+# Load the model from session_state
+model=st.session_state.model 
 
 st.title('Livestream object detection')
 
-def set_variable(value):
-    global variable
-    variable = value
+def display_video(model,stream_url):
+ 
+    draw_sidebar()
 
-if variable:
-    st.write("Variable is set to:", variable)
-    # run code with variable
-
-# Use GPU if available, else use CPU
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-boxes = torch.tensor([[0., 1., 2., 3.]]).to(device)
-scores = torch.randn(1).to(device)
-iou_thresholds = 0.5
-
-# YoloV5
-
-# load pretrained model locally
-#model = yolov5.load('/model/yolov5s.pt')
-
-# load pretrained model on streamlit
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/yolov5s.pt', force_reload=True) 
-model.to(device)  
-
-values=[]
-videos=[]
-x=int(0)
-
-urls=['https://www.youtube.com/watch?v=hIJs5p7ND0g',
-'https://www.youtube.com/watch?v=1-iS7LArMPA',
-'https://www.youtube.com/watch?v=CvOB-Is_yYU']
-
-videostream=urls[0]
-
-for url in urls:
-    # Use pafy to get videostream from youtube
-    videos.append(pafy.new(url))
-    # getting thumbnail of the video
-    values.append(videos[x].thumb)
-    x+=1
-
-# Create a sidebar for overview    
-x=int(0)
-st.sidebar.title("Youtube streams") 
-for value in values:
-    st.sidebar.image(value)
-    if st.sidebar.button(f"Stream {x}",x):
-        set_variable(urls[x]) 
-    x+=1
-
-# Catch the stop button
-if st.button('Stop'):
-    st.stop()
-
-# Wait for input, if a button is pressed, variable will have the value of the clicked button
-if variable:
-
-    videostream=variable
+    videostream=stream_url
     video=pafy.new(videostream)
-
-    # Print all available streams
-    streams = video.allstreams 
-    for s in streams:
-        print(s)
-
-    #best  = video.getbest() 
-    #print (best.resolution, best.extension)  
-
-    # Search for a resolution
+ 
+    # Search for a 640x360 resolution 
     desired_resolution = None
-    for s in video.allstreams:
-        if s.resolution == '640x360':
-            desired_resolution = s
-            break
-
-    if desired_resolution:
-        print(desired_resolution.resolution)
-        #best_resolution.download()
-    else:
-        print("No video stream matches the specified resolution")
+    for stream in video.allstreams:
+        if stream.resolution == '640x360':
+            desired_resolution = stream
+            break  
 
     # Capture with OpenCV
     capture = cv2.VideoCapture(desired_resolution.url)
     capture.set(cv2.CAP_PROP_FPS, 10)
     fps = capture.get(cv2.CAP_PROP_FPS)
 
-    # Print the frames in same window
+    # Display frames in same window
     image_place = st.empty()
     capture = cv2.VideoCapture(desired_resolution.url)
     while True:
@@ -142,31 +147,11 @@ if variable:
         image_place.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
         #time.sleep(.05)
-    
     capture.release()
     cv2.destroyAllWindows() 
 
-else:
-    with st.empty(): 
-        while True:
-            st.write(f"⏳ Please choose a stream")
-            time.sleep(0.31)
-            st.write(f"⏳ Please choose a stream .")
-            time.sleep(0.31)
-            st.write(f"⏳ Please choose a stream ..")
-            time.sleep(0.31)
-            st.write(f"⏳ Please choose a stream ...")
-            time.sleep(0.31)
-            st.write(f"⏳ Please choose a stream ....")
-
-st.text(torch.cuda.is_available())
-st.text(torch.__version__)
-st.text(torch.version.cuda)
-st.text(torchvision.__version__)
-
-"""True
-1.13.1+cu117
-11.7
-0.14.1+cu117
-"""
+# Catch the stop button
+if st.button('Stop'):
+    st.stop()
+ 
 
